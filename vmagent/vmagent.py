6a50536826e12fb776c3e4c9e8dbc544a4291afe
Tanya Lyubimaya <tatyana.lyubimaya.work@gmail.com>
2023-11-27 18:40:09 +0300

    ✨ add rpc cancellation on client side

--- vmagent/vmagent.py
+++ vmagent/vmagent.py
@@ -1,5 +1,6 @@
 import os
 import threading
+import time
 
 import grpc
 import sys
@@ -30,18 +31,31 @@ def call_collect_metrics():
 
 
 def run_server_streaming_client():
-    channel = grpc.insecure_channel('localhost:50051')  # Replace with your server address
+    channel = grpc.insecure_channel('localhost:50051')
     stub = exporter_pb2_grpc.ExporterStub(channel)
-
     request = google.protobuf.empty_pb2.Empty()
-
     stream = stub.StreamMetrics(request)
+    cancellation_event = threading.Event()
+
+    def cancel_stream_after_timeout():
+        time.sleep(10)  # cancel after 10 seconds
+        cancellation_event.set()
+
+    cancellation_thread = threading.Thread(target=cancel_stream_after_timeout)
+    cancellation_thread.start()
 
     try:
         for response in stream:
-            print("[STREAMING] received metrics response:", response)
+            if not cancellation_event.is_set():
+                print("[STREAMING] received metrics response:", response)
+            else:
+                print("cancelling the stream after timeout")
+                stream.cancel()
+                break
     except grpc.RpcError as e:
         print(f"error while reading stream: {e}")
+
+    cancellation_thread.join()  # wait for the cancellation thread to finish
 
 
 if __name__ == '__main__':
